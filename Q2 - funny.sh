4e25d3473c7615129a9bd370905fe05b5967948d
Nitinreddy1997 <62975537+Nitinreddy1997@users.noreply.github.com>
2021-03-17 15:31:25 +1100

    Create Q2 - funny.sh

---
+++ Q2 - funny.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#If an argument is not passed funny.sh will print "This is NOT funny" or else it prints "This is funny".
+
+if [ "$1" = "" ]; then
+        echo "This is NOT funny"
+        exit 0
+else
+        echo "This is funnny"
+fi
